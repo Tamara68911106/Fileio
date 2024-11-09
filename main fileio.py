456589ad0facd22ad83b123f55acae459bcd9a06
User <toma2006.06.11.@gmail.com>
2024-11-09 14:55:20 +0400

    Добавлена функция сохранения в формате json

--- main fileio.py
+++ main fileio.py
@@ -4,6 +4,20 @@ from tkinter import messagebox as mb
 from tkinter import ttk
 import requests
 import pyperclip
+import json
+import os
+
+history_file= "upload_history.json"
+
+def save_history(file_path, link): # функция будет принимать путь к файлу и ссылку
+    history = [] # изначально хистори - путой список
+    if os.path.exists(history_file): # проверяем, что путь к файлу существует в папке
+        with open(history_file, 'r') as f: # если сущ-т-открываем его для чтения
+            history=json.load(f) # загружаем,что было в нашем файле
+    history.append({"file_path": os.path.basename(file_path), "download_link": link}) # добавляем новую инфо в историю,
+    # т.к это словарь - это{}
+    with open(history_file, 'w') as f:
+        json.dump(history, f, indent=4)
 
 def upload(): #создаем функцию
     try:
@@ -17,6 +31,7 @@ def upload(): #создаем функцию
                 entry.delete(0,END)
                 entry.insert(0,link)
                 pyperclip.copy(link)
+                save_history(filepath, link)
                 mb.showinfo("Ссылка скопирована", f"Ссылка {link} успешно скопирована в буфер обмена")
     except Exception as e:
         mb.showerror("Ошибка", f"Произошла ошибка: {e}")
